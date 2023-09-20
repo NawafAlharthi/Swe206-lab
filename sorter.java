81294240210c55da50cd6c56c4ee40439eb94e51
Nawaf <nawafallharthii@gmail.com>
2023-09-20 13:24:08 +0300

    Last chnages

--- sorter.java
+++ sorter.java
@@ -25,22 +25,7 @@ class sorter{
             arr[minIdx] = temp;
         }
     }
-
-    public static void sort(int[] array) {
-        int n = array.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-
-            int temp = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = temp;
+        public static void sort(int[] array) {
+            selectionSort(array);
         }
-    }
 }
